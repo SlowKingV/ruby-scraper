ca604a97f2b8dd1fc5599c7f120e869a6400d40a
Diego Luna Granados <slowkingvii@gmail.com>
2020-03-16 00:50:57 -0600

    Add file handler for local or online sources

---
+++ lib/file_handler.rb
@@ -0,0 +1,24 @@
+class FileHandler
+  @current = nil
+  @repo_dir = Dir.open('repo/')
+  attr_reader :file, :file_path
+
+  def initialize(filename, dir = self.class.repo_dir)
+    @repo_dir = dir
+    @file_path = @repo_dir.path + filename + '.xml'
+    access = File.exist?(@file_path) ? 'r+' : 'w+'
+    load_file(access)
+  end
+
+  def load_file(access)
+    @file = File.new(@file_path, access)
+  end
+
+  class << self
+    attr_reader :repo_dir
+
+    def list_files
+      @repo_dir.entries.select { |entry| entry =~ /\.xml$/ }
+    end
+  end
+end
